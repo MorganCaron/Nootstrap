ec8b888c755bee9ae2180c63338fa9903d7e2656
MorganCaron <13729664+MorganCaron@users.noreply.github.com>
2021-06-07 06:31:17 +0000

    Deploying to gh-pages from @ MorganCaron/Nootstrap@334fd7a73d063857f3e61a36b89c9547c8e8f302 🚀

---
+++ src/ts/App.d.ts
@@ -0,0 +1,3 @@
+import 'nootstrap-ui';
+import 'prism-themes/themes/prism-atom-dark';
+//# sourceMappingURL=App.d.ts.map
--- src/ts/Nootstrap.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Nootstrap {
-    constructor();
-    updateParallax(): void;
-    onWindowResize(): void;
-    onWindowScroll(): void;
-    onTriggerClick(element: Element, event: Event): void;
-}
-//# sourceMappingURL=Nootstrap.d.ts.map
