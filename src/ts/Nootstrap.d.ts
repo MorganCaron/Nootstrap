dff922cd703a9a34526f199fad9df82ee5fec39a
MorganCaron <13729664+MorganCaron@users.noreply.github.com>
2021-06-07 03:56:44 +0000

    Deploying to gh-pages from @ MorganCaron/Nootstrap@dc5a4bd0bc19319b10970991bdc6909b156feaed 🚀

---
+++ src/ts/Nootstrap.d.ts
@@ -0,0 +1,8 @@
+declare class Nootstrap {
+    constructor();
+    updateParallax(): void;
+    onWindowResize(): void;
+    onWindowScroll(): void;
+    onTriggerClick(element: Element, event: Event): void;
+}
+//# sourceMappingURL=Nootstrap.d.ts.map
